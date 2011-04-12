83609e8016549e679a72248b83cd684dfad09e5b
gibson <forevergibson@gmail.com>
2011-04-12 15:16:36 +0800

    add connect

--- ChatClient.java
+++ ChatClient.java
@@ -1,6 +1,10 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.*;
 
 public class ChatClient extends Frame{
 	
@@ -25,7 +29,30 @@ public class ChatClient extends Frame{
 			}
 			
 		});
+		tfTxt.addActionListener(new TextFieldListener());
 		setVisible(true);
+		connect();
+	}
+	
+	public void connect() {
+		try {
+			Socket s = new Socket("127.0.0.1", 8999);
+System.out.println("connected!");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	private class TextFieldListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			
+			String s = tfTxt.getText().trim();
+			taContent.setText(s);
+			tfTxt.setText("");
+		}
+		
+	}
 }
