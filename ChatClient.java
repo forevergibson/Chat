35492db58a0b5f0e906aa92158808463b98e067f
gibson <forevergibson@gmail.com>
2011-04-12 08:27:04 +0800

    The second modify to ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -2,6 +2,9 @@ import java.awt.*;
 
 public class ChatClient extends Frame{
 	
+	TextField tfTxt = new TextField();
+	TextArea taContent = new TextArea();
+	
 	public static void main(String[] args) {
 		new ChatClient().launchFrame();
 	}
@@ -9,6 +12,9 @@ public class ChatClient extends Frame{
 	public void launchFrame() {
 		setLocation(400,300);
 		this.setSize(300,300);
+		add(tfTxt, BorderLayout.SOUTH);
+		add(taContent, BorderLayout.NORTH);
+		pack();
 		setVisible(true);
 	}
 
