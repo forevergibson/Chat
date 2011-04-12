fbf3585421a19c9da8a9121d1c06eb695a63bc1c
gibson <forevergibson@gmail.com>
2011-04-12 10:55:33 +0800

    add ChatServer.java

---
+++ ChatServer.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.*;
+
+
+public class ChatServer {
+
+	
+	public static void main(String[] args) {
+		try {
+			ServerSocket ss = new ServerSocket(8999);
+			while (true) {
+				Socket s = ss.accept();
+System.out.println("a client connected!");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
